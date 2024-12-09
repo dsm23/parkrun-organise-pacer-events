17cf78624f3ea88f473f94efcb49d5101bfb3633
David Murdoch <dsmurdoch357@gmail.com>
2024-12-09 16:54:04 +0000

    feat(select nodes): use select function on volunteer nodes
    * select nodes in protected route
    * remove tutorial components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 import Hero from "~/components/hero";
-import ConnectSupabaseSteps from "~/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "~/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "~/utils/supabase/check-env-vars";
 
 export default function Index() {
   return (
@@ -10,7 +7,6 @@ export default function Index() {
       <div className="flex flex-1 flex-col gap-6 px-4">
         <h1 className="sr-only">Delete this</h1>
         <h2 className="mb-4 text-xl font-medium">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
         <h2 className="mb-2 mt-4 text-xl font-medium">The Problem</h2>
         <p>
           Parkrun is an event where people get somewhat obsessed with their own
