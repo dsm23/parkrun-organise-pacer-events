5c23050a2319c16adca325c6b6a3151e99937e73
David Murdoch <dsmurdoch357@gmail.com>
2024-12-16 14:18:20 +0000

    feat(landing page): clean up landing page
    * remvoe hero section from template
    * fix grammar
    * add sign up and sign in buttons in landing page contents

--- playwright-tests/example.spec.ts
+++ playwright-tests/example.spec.ts
@@ -4,7 +4,7 @@ import { expect, test } from "@playwright/test";
 test("has title", async ({ page }) => {
   await page.goto("/");
 
-  await expect(page).toHaveTitle(/Next.js and Supabase Starter Kit/);
+  await expect(page).toHaveTitle(/Parkrun pacer events/);
 });
 
 test("has heading", async ({ page }) => {
@@ -12,7 +12,7 @@ test("has heading", async ({ page }) => {
 
   await expect(
     page.getByRole("heading", {
-      name: "Delete this",
+      name: "Parkrun pacer event organiser",
     }),
   ).toBeVisible();
 });
